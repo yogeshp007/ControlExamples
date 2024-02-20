5345b211e0b5a768d2772b91909a78543f48d351
yog-pande <70682331+yog-pande@users.noreply.github.com>
2024-02-20 08:18:51 +0530

    jq examples

---
+++ Controllers/JqExampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlExamples.Controllers
+{
+    public class JqExampleController : Controller
+    {
+        // GET: JqExample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetData()
+        {
+            var data = new { message = "this is from isro" };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
